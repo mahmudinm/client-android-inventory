d8082df48a5f5d6f46a03a62b4facca190db1e6b
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-11 20:50:27 +0700

    coba get data supplier tampil ke adapter

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/supplier/SupplierFragment.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/supplier/SupplierFragment.java
@@ -1,18 +1,40 @@
 package com.example.mahmudinm.androidcodeigniterinventory.view.supplier;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.mahmudinm.androidcodeigniterinventory.R;
+import com.example.mahmudinm.androidcodeigniterinventory.model.Supplier;
+import com.example.mahmudinm.androidcodeigniterinventory.network.response.SupplierResponse;
+import com.example.mahmudinm.androidcodeigniterinventory.utils.SessionManager;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SupplierFragment extends Fragment {
+public class SupplierFragment extends Fragment implements SupplierView{
+
+    ProgressDialog progressDialog;
+    SupplierPresenter presenter;
+//    RecyclerView.LayoutManager manager;
+    SessionManager session;
+    SupplierAdapter adapter;
+    List<Supplier> suppliers;
+
+    @BindView(R.id.recyclerSupplier)
+    RecyclerView recyclerView;
 
 
     public SupplierFragment() {
@@ -24,7 +46,41 @@ public class SupplierFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_supplier, container, false);
+        View x = inflater.inflate(R.layout.fragment_supplier, container, false);
+        session = new SessionManager(getActivity());
+        ButterKnife.bind(this, x);
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading ...");
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        presenter = new SupplierPresenter(this);
+        presenter.getSuppliers(session.getKeyToken());
+
+        return x;
     }
 
+    @Override
+    public void showProgress() {
+        progressDialog.show();
+    }
+
+    @Override
+    public void hideProgress() {
+        progressDialog.dismiss();
+
+    }
+
+    @Override
+    public void statusSuccess(SupplierResponse supplierResponse) {
+        adapter = new SupplierAdapter(supplierResponse.getData());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void statusError(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/supplier/SupplierPresenter.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/supplier/SupplierPresenter.java
@@ -1,4 +1,54 @@
 package com.example.mahmudinm.androidcodeigniterinventory.view.supplier;
 
+import com.example.mahmudinm.androidcodeigniterinventory.network.ApiClient;
+import com.example.mahmudinm.androidcodeigniterinventory.network.ApiInterface;
+import com.example.mahmudinm.androidcodeigniterinventory.network.response.SupplierResponse;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class SupplierPresenter {
+
+    SupplierView view;
+    ApiInterface apiInterface;
+    CompositeDisposable compositeDisposable;
+
+    public SupplierPresenter(SupplierView supplierView) {
+        this.view = supplierView;
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    public void getSuppliers(String token) {
+        view.showProgress();
+        compositeDisposable.add(
+                apiInterface.getSuppliers(token)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableObserver<SupplierResponse>(){
+                            @Override
+                            public void onNext(SupplierResponse supplierResponse) {
+                                if (supplierResponse.getStatus().equals("success")) {
+                                    view.statusSuccess(supplierResponse);
+                                } else {
+                                    view.statusError(supplierResponse.getStatus());
+                                }
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                view.hideProgress();
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                view.hideProgress();
+
+                            }
+                        })
+        );
+    }
+
 }
