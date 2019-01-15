38ebfd2c9cc5eba0fd0324a8bc447d4b54e7812f
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-15 11:13:20 +0700

    benerin dikit

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/barang/BarangFragment.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/barang/BarangFragment.java
@@ -1,22 +1,43 @@
 package com.example.mahmudinm.androidcodeigniterinventory.view.barang;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.mahmudinm.androidcodeigniterinventory.R;
+import com.example.mahmudinm.androidcodeigniterinventory.network.response.BarangResponse;
 import com.example.mahmudinm.androidcodeigniterinventory.utils.SessionManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BarangFragment extends Fragment {
+public class BarangFragment extends Fragment implements BarangView {
 
     SessionManager session;
+    BarangPresenter presenter;
+    BarangAdapter adapter;
+    ProgressDialog progressDialog;
 
+    @BindView(R.id.recycler)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @BindView(R.id.swipe_refresh)
+    SwipeRefreshLayout swipe;
 
     public BarangFragment() {
         // Required empty public constructor
@@ -28,8 +49,39 @@ public class BarangFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View x = inflater.inflate(R.layout.fragment_barang, container, false);
+        ButterKnife.bind(this, x );
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading ...");
+
+        session = new SessionManager(getActivity());
+        presenter = new BarangPresenter(this);
+        presenter.getBarang(session.getKeyToken());
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return x;
     }
 
+    @Override
+    public void showProgress() {
+        progressDialog.show();
+    }
+
+    @Override
+    public void hideProgress() {
+        progressDialog.dismiss();
+    }
+
+    @Override
+    public void statusSuccess(BarangResponse barangResponse) {
+        adapter = new BarangAdapter(barangResponse.getData(), getActivity());
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void statusError(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
 }
