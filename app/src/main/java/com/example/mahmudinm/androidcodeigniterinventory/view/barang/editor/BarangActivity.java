23850b9153ae4d6200f43a23c8f67e77fa17c5f3
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-16 13:10:26 +0700

    berhasil pick image dari camera

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/barang/editor/BarangActivity.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/barang/editor/BarangActivity.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.example.mahmudinm.androidcodeigniterinventory.BuildConfig;
 import com.example.mahmudinm.androidcodeigniterinventory.R;
 import com.example.mahmudinm.androidcodeigniterinventory.utils.SessionManager;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -66,16 +77,35 @@ public class BarangActivity extends AppCompatActivity {
                     public void onSelection(MaterialDialog dialog, View itemView, int position, CharSequence text) {
                         switch (position) {
                             case 0:
-                                Intent intent = new Intent();
-                                intent.setType("image/*");
-                                intent.setAction(Intent.ACTION_GET_CONTENT);
-                                startActivityForResult(intent.createChooser(intent, "Select Image"), REQUEST_GALLERY);
+                                Intent intentGallery = new Intent();
+                                intentGallery.setType("image/*");
+                                intentGallery.setAction(Intent.ACTION_GET_CONTENT);
+                                startActivityForResult(intentGallery.createChooser(intentGallery, "Select Image"), REQUEST_GALLERY);
                                 break;
+                            case 1:
+                                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                uri = ambilOutputMediaFileUri(type_foto_code);
+                                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                                startActivityForResult(intentCamera, REQUEST_CAMERA);
                         }
                     }
                 })
                 .show();
 
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_GALLERY && resultCode != 0) {
+            uri = data.getData();
+            gambar.setImageURI(uri);
+        } else if (requestCode == REQUEST_CAMERA && resultCode != 0) {
+            uri = Uri.parse(currentPhotoPath);
+            selectImagePath = uri.getPath();
+            gambar.setImageURI(uri);
+        }
     }
 
     private void permission() {
@@ -101,14 +131,52 @@ public class BarangActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_GALLERY && resultCode != 0) {
-            uri = data.getData();
-            gambar.setImageURI(uri);
-        }
+    private Uri ambilOutputMediaFileUri(int type_foto_code) {
+        // mengambil alamat directory file
+        // return Uri.fromFile(ambilOutputMediaFile(type_foto_code));
+        return FileProvider.getUriForFile(BarangActivity.this,
+                BuildConfig.APPLICATION_ID + ".provider",
+                ambilOutputMediaFile());
     }
 
+    private File ambilOutputMediaFile() {
+        // atur alamat penyimpanan (SDCard/Pictures/folder_foto)
+        File penyimpananMediaDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                , folder
+        );
+        Log.d("Directory Fileku", penyimpananMediaDir.getAbsolutePath());
+
+
+        // cek keberadaan folder
+        if (!penyimpananMediaDir.exists()) {
+            // cek jika tidak bisa membuat folder baru
+            if (!penyimpananMediaDir.mkdir()) {
+                Toast.makeText(this, "Gagal membuat folder "
+                        + folder, Toast.LENGTH_SHORT).show();
+                return null;
+            }
+        }
+
+        // simpan format tanggal saat pengambilan foto
+        String waktu = new SimpleDateFormat("yyyyMMdd_hhMss"
+                , Locale.getDefault()).format(new Date());
+        Log.d("Waktu Pengambilan", waktu);
+
+        // variabel penampung nama file
+        File mediaFile;
+        // pasang nama foto dengan waktu
+        if (type_foto_code == type_foto_code) {
+            mediaFile = new File(penyimpananMediaDir.getPath() + File.separator
+                    + "IMG" + waktu + ".jpg");
+            Log.d("Nama FIle", mediaFile.getAbsolutePath());
+        } else {
+            return null;
+        }
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = "file:" + mediaFile.getAbsolutePath();
+        Log.d("currentPhotoPath : ", currentPhotoPath);
+        return mediaFile;
+    }
 
 }
