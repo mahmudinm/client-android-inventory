509acbbc045a54534ed6beb07e6e125d18d74cf1
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-16 11:29:50 +0700

    setup manifest dan permission

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/barang/editor/BarangActivity.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/barang/editor/BarangActivity.java
@@ -1,15 +1,58 @@
 package com.example.mahmudinm.androidcodeigniterinventory.view.barang.editor;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.mahmudinm.androidcodeigniterinventory.R;
 
+import butterknife.BindView;
+
 public class BarangActivity extends AppCompatActivity {
+
+    @BindView(R.id.kode)
+    EditText kode;
+    @BindView(R.id.nama)
+    EditText nama;
+    @BindView(R.id.stock)
+    EditText stock;
+    @BindView(R.id.harga)
+    EditText harga;
+    @BindView(R.id.ukuran)
+    EditText ukuran;
+    @BindView(R.id.gambar)
+    ImageView gambar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barang);
+    }
+
+    private void permission() {
+        if (
+                Build.VERSION.SDK_INT >= 23
+                        && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED
+                        && checkSelfPermission(Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED
+                        && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED
+                        && checkSelfPermission(Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED
+                ) {
+            requestPermissions(new String[]{
+                    Manifest.permission.RECORD_AUDIO
+                    , Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    , Manifest.permission.READ_EXTERNAL_STORAGE
+                    , Manifest.permission.CAMERA
+            }, 0);
+        } else {
+
+        }
     }
 }
