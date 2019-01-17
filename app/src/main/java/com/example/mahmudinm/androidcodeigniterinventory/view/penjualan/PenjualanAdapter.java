9d090b2a2476c1500092f611bc41570dbe3312c8
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-17 11:09:34 +0700

    setup penjualan

---
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/penjualan/PenjualanAdapter.java
@@ -0,0 +1,33 @@
+package com.example.mahmudinm.androidcodeigniterinventory.view.penjualan;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PenjualanAdapter extends RecyclerView.Adapter<PenjualanAdapter.ViewHolder> {
+
+    
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/penjualan/PenjualanView.java
@@ -0,0 +1,4 @@
+package com.example.mahmudinm.androidcodeigniterinventory.view.penjualan;
+
+public interface PenjualanView {
+}
