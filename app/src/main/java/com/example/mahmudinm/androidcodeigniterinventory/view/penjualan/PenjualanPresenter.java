d582498031344a885c2094e579387c8f0c01b3cb
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-17 11:21:43 +0700

    setup view presenter penjualan

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/penjualan/PenjualanPresenter.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/penjualan/PenjualanPresenter.java
@@ -1,4 +1,50 @@
 package com.example.mahmudinm.androidcodeigniterinventory.view.penjualan;
 
+import com.example.mahmudinm.androidcodeigniterinventory.network.ApiClient;
+import com.example.mahmudinm.androidcodeigniterinventory.network.ApiInterface;
+import com.example.mahmudinm.androidcodeigniterinventory.network.response.PenjualanResponse;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class PenjualanPresenter {
+
+    PenjualanView view;
+    ApiInterface apiInterface;
+    CompositeDisposable compositeDisposable;
+
+    public PenjualanPresenter(PenjualanView view) {
+        this.view = view;
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    public void getBarang(String token) {
+        view.showProgress();
+        compositeDisposable.add(
+                apiInterface.getPenjualan(token)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableObserver<PenjualanResponse>(){
+                            @Override
+                            public void onNext(PenjualanResponse penjualanResponse) {
+                                view.statusSuccess(penjualanResponse);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                view.hideProgress();
+                                view.statusError(e.getLocalizedMessage());
+                            }
+
+                            @Override
+                            public void onComplete() {
+                                view.hideProgress();
+                            }
+                        })
+        );
+    }
+
 }
--- app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/penjualan/PenjualanView.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterinventory/view/penjualan/PenjualanView.java
@@ -1,4 +1,12 @@
 package com.example.mahmudinm.androidcodeigniterinventory.view.penjualan;
 
+import com.example.mahmudinm.androidcodeigniterinventory.network.response.PenjualanResponse;
+
 public interface PenjualanView {
+
+    void showProgress();
+    void hideProgress();
+    void statusSuccess(PenjualanResponse penjualanResponse);
+    void statusError(String message);
+
 }
